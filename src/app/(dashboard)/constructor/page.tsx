9bb6438820a57f2550285294c38004425813c41f
oles.potaskalov <alexandr.potaskalov@gmail.com>
2025-02-09 02:26:16 +0200

    ver for scene-list ?

--- src/app/(dashboard)/constructor/page.tsx
+++ src/app/(dashboard)/constructor/page.tsx
@@ -1,9 +1,10 @@
 import { LuMousePointerClick } from "react-icons/lu";
-import { MdCreateNewFolder } from "react-icons/md";
+import { MdAddBox, MdCreateNewFolder, MdNoteAdd } from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
-import { IoIosPlay } from "react-icons/io";
+import { IoIosPlay, IoMdAddCircleOutline } from "react-icons/io";
 
 import styles from './styles.module.scss';
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const ConstructorPage = () => {
     return (
@@ -32,7 +33,27 @@ const ConstructorPage = () => {
                     </div>
                     <div className={styles['scene__side']}></div>
                 </div>
-                <div className={`${styles['container']} ${styles['scene-list']}`}></div>
+                <ScrollArea className={`${styles['scroll']} ${styles['container']} dark`}>
+                    <div className={`${styles['scene-list']}`}>
+                        <div className={`${styles['item']} ${styles['new-item']}`}>
+                            <MdAddBox />
+                        </div>
+                        {
+                            [...new Array(10)].map(
+                                (_, index) => (
+                                    <div key={index} className={styles['item']}>
+                                        <img
+                                            className={styles['item__img']}
+                                            src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9cc76151-95e3-4c69-b7db-f48f73ea854f/da86jda-8dac0c26-fc87-4f07-9dd4-b10efb097776.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzljYzc2MTUxLTk1ZTMtNGM2OS1iN2RiLWY0OGY3M2VhODU0ZlwvZGE4NmpkYS04ZGFjMGMyNi1mYzg3LTRmMDctOWRkNC1iMTBlZmIwOTc3NzYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.k4jCx0z9BaZRLQ82G7IUrS3qXD1L7utHsGn0hcIncyE'}
+                                        />
+                                        <label>Scene 52341</label>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                    <ScrollBar className="m-1" orientation="horizontal"></ScrollBar>
+                </ScrollArea>
             </div>
         </div>
     );
